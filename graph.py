1b4b8ac18f699e3c23c2b86df5d726d234b48e8b
Sameer Patel <spatel78745@gmail.com>
2016-01-01 14:10:31 -0500

    added dfs

--- graph.py
+++ graph.py
@@ -31,22 +31,38 @@ class Graph(dict):
                 self.addEdge(int(vertices[0]), int(vertices[1]))
                 
     def adj(self, v): return self[v]
-
-def testGraph():
-    g = Graph()
-    g.addEdge(0, 1)
-    g.addEdge(1, 2)
-    g.addEdge(0, 2)
-    print(g, g.V, g.E)
     
-    g1 = Graph('/Users/spatel78745/py/tinyG.txt')
-    print(g1, g1.V, g1.E)
-
-    print('Vertices adjacent to 0')
-    for v in g1.adj(0):
-        print(v)
+class DepthFirstSearch:
+    def __init__(self, G, s):
+        self.G = G
+        self.count = 0
+        self.marked = {}
+        self.dfs(G, s)
     
-testGraph()
+    def dfs(self, G, v):
+        self.marked[v] = True
+        for w in G[v]:
+            if w not in self.marked: self.dfs(G, w)
+
+# def testGraph():
+# g = Graph()
+# g.addEdge(0, 1)
+# g.addEdge(1, 2)
+# g.addEdge(0, 2)
+# print(g, g.V, g.E)
+
+g1 = Graph('/Users/spatel78745/py/tinyG.txt')
+# print(g1, g1.V, g1.E)
+
+# print('Vertices adjacent to 0')
+# for v in g1.adj(0):
+#     print(v)
+
+print('Searching: ', g1)    
+search = DepthFirstSearch(g1, 9)
+print('Vertices connected to', 9, ':', search.marked.keys())    
+    
+# testGraph()
 
 if __name__ == '__main__':
     pass
